4638d2c3bc9a02e1f3ca11e9bc9fe97e0ede34e3
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-07-03 19:05:54 +0530

    basic input output

--- src/BasicIOAnsVariable/BasicPrintScan.java
+++ src/BasicIOAnsVariable/BasicPrintScan.java
@@ -1,7 +1,9 @@
 package BasicIOAnsVariable;
-
+import java.util.Scanner;
 public class BasicPrintScan {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("My name is: "+name );
     }
 }
